c3dc412fe76ce57c714c23749e74ed0693bfd773
MicrosysX <aezaz@microsysx.com>
2024-11-18 09:51:33 +0500

    setting up login page and auth services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './laoyout/layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home", component:LayoutComponent
+    },
+    {
+        path:"login", component:LoginComponent
+    },
+];
